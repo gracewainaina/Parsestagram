779d1cce712d9b9657f6ae87992bbd4149a361ed
Grace Wainaina <7gracewainaina@gmail.com>
2020-07-07 13:43:46 -0700

    Log in, post, view feed
    -Enable user to sign in and stay signed in
    -User can post a photo taken by the camera
    -User can view the last 20 posts

---
+++ Parsestagram/app/src/main/java/com/example/parsestagram/LoginActivity.java
@@ -0,0 +1,74 @@
+package com.example.parsestagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public static final String TAG = "LoginActivity";
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // check if user is already logged in after opening the app again
+        // if so, go to main activity
+        if (ParseUser.getCurrentUser() != null){
+            goMainActivity();
+        }
+
+        etPassword = findViewById(R.id.etPassword);
+        etUsername = findViewById(R.id.etUsername);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick login button");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username, password);
+
+            }
+        });
+    }
+
+    private void loginUser(String username, String password){
+        Log.i(TAG, "attempt to log in user"+ username);
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                // Check if there is an issue with login
+                if (e != null){
+                    Log.e(TAG, "issue with login", e);
+                    Toast.makeText(LoginActivity.this, "Issue with login!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // Upon logging in properly, navigate to the main activity using intent
+                goMainActivity();
+                Toast.makeText(LoginActivity.this, "Login Success!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
